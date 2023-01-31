7931896e5b6a4691b5547c2d11ff87024e5521a0
roylee0912 <leeroy0912@gmail.com>
2023-01-31 14:45:10 -0500

    finished making it responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -34,29 +34,31 @@ const Home = ({ user }) => {
   }
 
   return (
-    <div className="home">
-      <div className="home-left">
-        <h4>LET'S WRITE THE BEST ESSAY EVER</h4>
-        <h2>Every Essay Can Use a Little Hepl</h2>
-        <img src={squiggle} className="squiggle"></img>
-        <h3>Get Your Essay Reviewed Today</h3>
-        <div className="home-btns">
-          <Link to="/login">
-            <button className="home-btn review-btn">Review Essays</button>
-          </Link>
-          <Link to="/login">
-            <button className="home-btn my-essays-btn">See My Essays</button>
-          </Link>
+      <div className="home">
+        <div className="home-left">
+          <h4>LET'S WRITE THE BEST ESSAY EVER</h4>
+          <h2>Every Essay Can Use a Little Hepl
+              <img src={squiggle} className="squiggle"></img>
+          </h2>
+        
+          <h3>Get Your Essay Reviewed Today</h3>
+          <div className="home-btns">
+            <Link to="/review">
+              <button className="home-btn review-btn">Review Essays</button>
+            </Link>
+            <Link to="/my-essays">
+              <button className="home-btn my-essays-btn">See My Essays</button>
+            </Link>
+          </div>
+        </div>
+        <div className="home-right">
+          <div className="hero-blob"></div>
+          <div className="hero-img-container">
+            <img src={hero}></img>
+          </div>
         </div>
       </div>
-      <div className="home-right">
-        <div className="hero-blob"></div>
-        <div className="hero-img-container">
-          <img src={hero}></img>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
