bfc167e3dce674b5bcd1306777d8ff0dc8dcb6a9
roylee0912 <leeroy0912@gmail.com>
2023-02-01 10:34:36 -0500

    added pg extension to schema

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require "active_model_serializers"
-
 module Revieword
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
