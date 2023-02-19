287838efb99afa0728df340fd1fb4d6226ab2c7e
roylee0912 <leeroy0912@gmail.com>
2023-02-19 18:50:03 -0500

    took true out of a string 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:4000'
     resource '*', headers: :any, 
                 methods: [:get, :post, :patch, :delete], 
-                credentials: 'true'
+                credentials: true
   end
   # nothing changed
 end
