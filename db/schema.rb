9672762fc8719dd6caace2ab6cb928c1fb626efb
roylee0912 <leeroy0912@gmail.com>
2023-02-20 02:17:51 -0500

    Revert " switched back to sqlite3, cant get postgres deployment to work"
    This reverts commit 824e17e0a7107c5c20faf9311abe122a612e4a66.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_25_160908) do
+    enable_extension "plpgsql"
+
+    
   create_table "essays", force: :cascade do |t|
     t.text "content"
     t.text "prompt"
