3329186d2a925db35e5abeca101aafde165155d4
roylee0912 <leeroy0912@gmail.com>
2023-02-19 19:31:37 -0500

    added some config in the routes 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
-
-  
   use Rack::Session::Cookie,
     :httponly => true
     :same_site => :none
     :secure => true
+
+Rails.application.routes.draw do
+
+
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
