67a0f79a45b122e1b601af64116172b9e53b9b19
Roy Lee <leeroy0912@gmail.com>
2023-02-21 16:10:45 -0500

    switched to postgres, deleted proxy

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_25_160908) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "essays", force: :cascade do |t|
     t.text "content"
     t.text "prompt"
@@ -19,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_160908) do
     t.text "overall_comments"
     t.string "length"
     t.boolean "is_reviewed"
-    t.integer "user_id", null: false
-    t.integer "reviewer_id"
+    t.bigint "user_id", null: false
+    t.bigint "reviewer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["reviewer_id"], name: "index_essays_on_reviewer_id"
