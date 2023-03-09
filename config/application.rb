8e42f0f5435f104a71e2a78aa225667fe387a5bd
roylee0912 <leeroy0912@gmail.com>
2023-03-09 09:42:58 -0500

    added some middleware code in the application rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module Revieword
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
+    
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch.cookies_same_site_protection=:strict
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
