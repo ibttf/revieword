01b40c5d0d47ab7a22824e948ac95ae97abcc71f
roylee0912 <leeroy0912@gmail.com>
2023-08-13 14:31:10 -0700

    deleted ruby files

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-    has_many :essays
-    has_many :reviewers, through: :essays
-    has_secure_password
-
-    validates :username, presence: true, uniqueness: true
-end
