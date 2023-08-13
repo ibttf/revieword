a0d670fbe5aa9a39750f2f10705212eb7fbde106
roylee0912 <leeroy0912@gmail.com>
2023-08-13 15:21:58 -0700

    redid baseurl

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,3 @@
 // const config={baseUrl: "https://shrouded-oasis-87140.herokuapp.com"}
-const config={baseUrl: "https://revieword-backend-express-c6596a3ac092.herokuapp.com"}
+const config={baseUrl: "https://revieword-backend.onrender.com"}
 export default config
